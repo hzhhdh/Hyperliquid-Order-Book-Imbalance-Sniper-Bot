42e5584c2442c7a91993dd0651f0ce8abb6d7584
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-04-07 14:18:21 +0200

    Delete exclusive_pool.rs

--- exclusive_pool.rs
+++
@@ -1,8 +0,0 @@
-impl FarmingBot {
-    pub fn enter_pool(&self, pool: Pool) -> Result<Transaction, PoolError> {
-        if pool.apr < self.min_apr || pool.tvl < self.min_tvl {
-            return Err(PoolError::NotProfitable);
-        }
-        self.submit_tx(pool.entry_function())
-    }
-}
