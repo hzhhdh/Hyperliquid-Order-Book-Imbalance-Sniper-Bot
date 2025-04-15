e589bb425035a8fab59e747bffd4a135883c4a5a
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-04-16 01:23:07 +0200

    Update network.py

--- network.py
+++ network.py
@@ -3,14 +3,13 @@ from config import CONFIG
 
 def get_rpc_provider():
     network = CONFIG["SELECTED_NETWORK"]
-    # Check for user custom RPC endpoints first:
     if network in CONFIG["CUSTOM_RPC_ENDPOINTS"] and CONFIG["CUSTOM_RPC_ENDPOINTS"][network]:
         rpc_url = CONFIG["CUSTOM_RPC_ENDPOINTS"][network]
     else:
         rpc_url = CONFIG["AVAILABLE_NETWORKS"].get(network)
     if not rpc_url:
         raise ValueError(f"No RPC endpoint found for network: {network}")
-    print(f"Connecting to {network} using RPC: {rpc_url}")
+    print(f"Connecting to {network} using RPC endpoint: {rpc_url}")
     return Web3(Web3.HTTPProvider(rpc_url))
 
 w3 = get_rpc_provider()
