7c7648187c19854dc6bd5f1c8f3776a269cf9551
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-04-16 01:22:50 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -6,11 +6,14 @@ def load_config(filename="config.json"):
         with open(filename, "r") as f:
             user_config = json.load(f)
         CONFIG.update(user_config)
-        print("Configuration successfully loaded.")
+        print("Configuration successfully loaded from", filename)
     except Exception as e:
-        print(f"Using default configuration due to error: {e}")
+        print("Using default configuration due to error:", e)
     return CONFIG
 
 def update_config(key, value):
     CONFIG[key] = value
     print(f"Config parameter {key} updated to {value}.")
+
+if __name__ == "__main__":
+    load_config()
