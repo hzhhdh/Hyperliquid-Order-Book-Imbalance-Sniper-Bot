52c2479093d26d6379aa418c7a67d4fe5c8cd087
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-04-16 11:47:22 +0200

    Delete engine directory

--- engine/ransaction_decoder.py
+++
@@ -1,9 +0,0 @@
-from dex_loader import dex_contract
-
-def decode_tx_input(input_data):
-    # This function uses the contract instance to decode the transaction data
-    try:
-        func_obj, params = dex_contract.decode_function_input(input_data)
-        return func_obj, params
-    except Exception as e:
-        raise Exception(f"Decoding failed: {e}")
