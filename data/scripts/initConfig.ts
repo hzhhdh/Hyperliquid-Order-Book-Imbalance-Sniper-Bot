dd35c5421c5c2a1263f957466c3a2475a1c88d20
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-04-24 01:35:02 +0200

    Delete data directory

--- data/scripts/initConfig.ts
+++
@@ -1,8 +0,0 @@
-import { existsSync, copyFileSync } from "fs";
-
-const configFile = "config.ts";
-const configTemplateFile = "config.mainnet.ts";
-
-if (!existsSync(configFile)) {
-  copyFileSync(configTemplateFile, configFile);
-}
