d459e99906ebdddf25b47237afafadeea0f66805
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-04-24 07:52:53 +0200

    Delete electron directory

--- electron/builder.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  appId: 'com.definexus',
-  productName: 'DeFiNexus',
-  directories: { output: 'dist' },
-  files: ['build/**/*', 'electron/**/*.js'],
-  mac: { target: 'dmg' },
-  win: { target: 'nsis' },
-};
--- electron/preload.ts
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  sendTrade: (order: any) => ipcRenderer.send('execute-trade', order),
-  onPriceUpdate: (cb: Function) => ipcRenderer.on('price-update', (_, data) => cb(data)),
-});
