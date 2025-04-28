411d36346e550c71ec53b6cd4bedd352119ad949
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-04-28 09:25:30 +0200

    Delete bot directory

--- bot/strategies/mev.rs
+++
@@ -1,9 +0,0 @@
-use ethers::{prelude::*, providers::Provider};
-use flashbots::FlashbotsMiddleware;
-
-async fn mev_protected_swap(provider: Provider<Http>, signer: LocalWallet) {
-    let fb = FlashbotsMiddleware::new(provider, signer, "https://relay.flashbots.net");
-    let tx = TransactionRequest::pay("vitalik.eth", 100).gas(21000);
-    fb.send_transaction(tx).await.unwrap();
-}
-
