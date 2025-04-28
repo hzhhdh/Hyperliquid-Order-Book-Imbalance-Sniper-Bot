78acd46ddd11a89e5c5633519cf9e44090aa2b51
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-04-28 09:25:55 +0200

    Delete scripts directory

--- scripts/configure_params.js
+++
@@ -1,7 +0,0 @@
-const { ethers } = require("hardhat");
-
-async function configure() {
-    const bot = await ethers.getContract("uniarb");
-    await bot.setArbitrageThreshold(500); // 5% threshold
-    console.log("Parameters configured");
-}
