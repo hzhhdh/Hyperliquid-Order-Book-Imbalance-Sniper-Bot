cddcc7a83944c407695170ee6e90ac0888c8655d
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-05-27 20:19:08 +0200

    Delete gas_optimizer.py

--- gas_optimizer.py
+++
@@ -1,4 +0,0 @@
-async def optimize_gas(chain_id: str, tx: dict) -> dict:
-    gas_price = await fetch_gas_price_from_api(chain_id)
-    tx['gasPrice'] = min(gas_price * 1.2, max_gas_price)
-    return tx
