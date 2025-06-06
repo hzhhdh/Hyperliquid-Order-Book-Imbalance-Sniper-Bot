328548c6cdbb47be1819eef6a1166337e8562f5a
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-06-06 12:44:06 +0200

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from config import *
-from sniper_bot import SniperBot
-
-if __name__ == "__main__":
-    bot = SniperBot(PRIVATE_KEY, API_URL, COINS, PRICE_RANGE, IMBALANCE_RATIO, MIN_NOTIONAL)
-    bot.run()
