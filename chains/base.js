95aea58824a6ba5f1e948c0985fd54be3eb3cea9
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-07-04 08:03:14 +0200

    Create base.js

---
+++ chains/base.js
@@ -0,0 +1,10 @@
+const Web3 = require('web3');
+const config = require('../../config/config');
+
+const web3 = new Web3(config.networks.base.rpc);
+
+function getWeb3Instance() {
+  return web3;
+}
+
+module.exports = { getWeb3Instance };
